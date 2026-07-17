728644099a30b6c095ad823149cfedd50c02c80e
mamad <you@example.com>
2026-07-17 22:55:10 +0330

    turn my database from db.sqlite to postgresql

--- src/mp/settings.py
+++ src/mp/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
+from . import private_variables
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,10 +82,16 @@ WSGI_APPLICATION = 'mp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "my_django_db",
+        'USER': "postgres",
+        'PASSWORD': private_variables.password,
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
