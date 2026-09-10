ad8d8f7d2c7bf9adb4780fbfae79e37aa294687c
mamad <you@example.com>
2026-09-10 22:43:32 +0330

    Update Railway allowed hosts

--- src/mp/settings.py
+++ src/mp/settings.py
@@ -35,12 +35,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = [
-    "172.25.87.239",
-    "localhost",
-    "127.0.0.1",
-    "onlin-shop-production-0b1f.up.railway.app",
-]
+ALLOWED_HOSTS = ["172.25.87.239", "localhost", "127.0.0.1","onlin-shop-production.up.railway.app", "onlin-shop-production-0b1f.up.railway.app"]
+
 
 # Application definition
 
