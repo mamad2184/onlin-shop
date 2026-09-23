d0b73c484b629c7b291662bb9376c70ebc7b9b4a
mamad <you@example.com>
2026-09-23 19:29:21 +0330

    using render for backend instade of railway

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
 
+//// for using Railway
+// const API_BASE = import.meta.env.VITE_API_BASE || (
+//   import.meta.env.DEV ? '/' : 'https://onlin-shop-production.up.railway.app/'
+// )
+
+
+// for using Render
 const API_BASE = import.meta.env.VITE_API_BASE || (
-  import.meta.env.DEV ? '/' : 'https://onlin-shop-production.up.railway.app/'
+  import.meta.env.DEV ? '/' : 'https://onlin-shop-6bgu.onrender.com/'
 )
+
+
 const token = localStorage.getItem('access_token')
 
 const api = axios.create({
